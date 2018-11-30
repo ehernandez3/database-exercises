4d84930ee7e7b1629b5a75fb8025b4891287088b
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-11-29 22:40:10 -0600

    completed tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -20,10 +20,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) NOT NULL,
-    name VARCHAR(50) NOT NULL,
+    artist VARCHAR(55) NOT NULL,
+    name VARCHAR(55) NOT NULL,
     release_date INT NOT NULL,
     sales FLOAT,
-    genre VARCHAR(50),
+    genre VARCHAR(55),
     PRIMARY KEY (id)
 )
