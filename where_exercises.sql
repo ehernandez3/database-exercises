63d81d0609cdbd3c637073420f1e862fd031acc8
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-12-03 10:55:28 -0600

    completed #4, #5 and #6 from first part of exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -9,8 +9,13 @@ SELECT * FROM employees
 WHERE last_name LIKE 'E%';
 
 #4 Find all employees hired in the 90s — 135,214 rows.
-
+SELECT * FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
 #5 Find all employees born on Christmas — 842 rows.
+SELECT * FROM employees
+WHERE birth_date LIKE '%-12-25';
 
 #6 Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT * FROM employees
+WHERE last_name LIKE '%q%';
