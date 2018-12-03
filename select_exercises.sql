af630f6c4bee90ecaab0241b19863c6577097c07
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-12-02 19:07:52 -0600

    added update_exercises.sql

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,40 +1,35 @@
 use codeup_test_db;
 
 -- The name of all albums by Pink Floyd.
-select 'The name of all albums by Pink Floyd.' as 'Pink Floyd Albums';
-
-SELECT name FROM albums
+SELECT name AS 'Pink Floyd Albums'
+FROM albums
 WHERE artist = 'Pink Floyd';
 
--- The year Sgt. Pepper's Lonely Hearts Club Band was released
-SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Sgt. Peppers''s Release Date';
 
-SELECT release_date FROM albums
+-- The year Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT release_date AS 'Sgt. Peppers''s Release Date'
+FROM albums
 WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 -- The genre for Nevermind
-SELECT 'The genre for Nevermind' as 'Nevermind Genre';
-
-SELECT genre FROM albums
+SELECT genre AS 'Nevermind Genre'
+FROM albums
 WHERE name = 'Nevermind';
 
 -- Which albums were released in the 1990s
-SELECT 'Albums relased in the 1900s' AS '1900s Albums';
-
-SELECT name FROM albums
+SELECT name AS '1900s Albums'
+FROM albums
 WHERE release_date BETWEEN 1990 AND 1999;
 
 -- Which albums had less than 20 million certified sales
-SELECT 'Albums with less than 20 million certified sales' AS 'Sales under 20 million';
-
-SELECT name FROM albums
+SELECT name AS 'Sales under 20 million'
+FROM albums
 WHERE sales < 20;
 
 -- All the albums with a genre of "Rock". Why do these query results
 -- not include albums with a genre of "Hard rock" or "Progressive rock"?
-SElECT 'All the albums with a genre of "Rock". N' AS 'Rock Genre Albums';
-
-SELECT * FROM albums
+SELECT name AS 'Rock Genre Albums'
+FROM albums
 WHERE genre = 'Rock';
 
 # SELECT * FROM albums
@@ -43,7 +38,7 @@ WHERE genre = 'Rock';
 
 
 -- As always, commit your changes after each step and push them out to GitHub
-# SELECT * FROM albums;
+SELECT * FROM albums;
 
 # Create a new file called select_exercises.sql.
 #
